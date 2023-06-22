e0a1b1f464118a91f710a71d81846259816a3380
Krystian Jagoda <krystian.jagoda@outlook.com>
2023-06-22 20:48:28 +0200

    modify files and add first component: header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style.css';
+import React from "react";
+import Header from "./Components/Header";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+const App = () => {
+  return <div>
+      <Header />
+  </div>
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    const title = `Weather App in React`;
+    return <div className="heading">
+        `test: ${title}`
+    </div>
+}
+export default Header;
+
